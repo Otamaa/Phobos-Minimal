4b7b46e11f37f4cb6766bd9f95810a8ca3bd2718
Otamaa <otamaindodapuq2@gmail.com>
2023-11-05 00:11:58 +0700

    - splits the IsoTile `CustomPalette` hook , for safety reason

--- src/Ext/IsometricTileType/Hooks.cpp
+++ src/Ext/IsometricTileType/Hooks.cpp
@@ -5,25 +5,54 @@
 #include <CellClass.h>
 #include <ConvertClass.h>
 
-DEFINE_HOOK(0x544E70, IsometricTileTypeClass_Init_Drawer, 0x8)
+LightConvertClass* GetConvert(CellClass* pCell, int r, int g, int b)
 {
-	GET(CellClass*, pCell, ESI); // Luckily, pCell is just ESI, so we don't need other hooks to set it
+	if (!DSurface::Primary())
+		return nullptr;
 
-	GET(int, red, ECX);
-	GET(int, green, EDX);
-	GET_STACK(int, blue, 0x4);
-
-	int isoTileTypeIndex = pCell->IsoTileTypeIndex;
-
-	if (auto pIso = IsometricTileTypeClass::Array->GetItemOrDefault(isoTileTypeIndex))
-	{
+	if (auto pIso = IsometricTileTypeClass::Array->GetItemOrDefault(pCell->IsoTileTypeIndex)) {
 		const auto pExt = IsometricTileTypeExtContainer::Instance.Find(pIso);
 
-		if(pExt->Palette){
-			R->EAX(pExt->GetLightConvert(red, green, blue));
-			return 0x544FDE;
+		if (pExt->Palette) {
+			return pExt->GetLightConvert(r, g, b);
 		}
 	}
 
-	return 0;
+	return LightConvertClass::InitLightConvert(r, g, b);
+}
+
+DEFINE_HOOK(0x483F5B, CellClass_InitDrawer_custom_1,0x8)
+{
+	GET(CellClass*, pCell, ESI);
+	GET_STACK(int, b, 0x18 - 0x10);
+	GET_STACK(int, g, 0x18 - 0x14);
+	GET_STACK(int, r, 0x18 + 0x4);
+
+	R->EAX(GetConvert(pCell, r,g,b));
+	return 0x483F6D;
+}
+
+DEFINE_HOOK(0x483FE5, CellClass_InitDrawer_custom_2, 0x5)
+{
+	GET(CellClass*, pCell, ESI);
+	R->EAX(GetConvert(pCell, 1000, 1000, 1000));
+	return 0x483FEF;
+}
+
+DEFINE_HOOK(0x484135, CellClass_UpdateCellLightning_custom_1, 0x8)
+{
+	GET(CellClass*, pCell, ESI);
+	GET_STACK(int, b, 0x14 + 0x10);
+	GET_STACK(int, g, 0x14 + 0x14);
+	GET_STACK(int, r, 0x14 - 0x4);
+
+	R->EAX(GetConvert(pCell, r, g, b));
+	return 0x484147;
+}
+
+DEFINE_HOOK(0x484155, CellClass_UpdateCellLightning_custom_2, 0x5)
+{
+	GET(CellClass*, pCell, ESI);
+	R->EAX(GetConvert(pCell, 1000, 1000, 1000));
+	return 0x484166;
 }
