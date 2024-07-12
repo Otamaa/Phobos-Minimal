7691491a4291ab63a10022c84de8781fb9d05174
Otamaa <otamaindodapuq2@gmail.com>
2024-07-12 23:16:18 +0700

    - remove unused hook

--- src/Ext/Tactical/Hooks.cpp
+++ src/Ext/Tactical/Hooks.cpp
@@ -1,8 +1,8 @@
 #include "Body.h"
 #include <Ext/Rules/Body.h>
 
-DEFINE_HOOK(0x6D4CD9, PrintTimerOnTactical_BlinkColor, 0x6)
-{
-	R->EDI(ColorScheme::Array->Items[RulesExtData::Instance()->TimerBlinkColorScheme]);
-	return 0x6D4CE2;
-}
+//DEFINE_HOOK(0x6D4CD9, PrintTimerOnTactical_BlinkColor, 0x6)
+//{
+//	R->EDI(ColorScheme::Array->Items[RulesExtData::Instance()->TimerBlinkColorScheme]);
+//	return 0x6D4CE2;
+//}
