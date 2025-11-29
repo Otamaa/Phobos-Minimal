fe15d186f2b1f50627f633d2e7046625ac5fd2a2
Otamaa <otamaindodapuq2@gmail.com>
2025-11-29 11:17:57 +0700

    - log what FPU rounding mode is used - provide backport implementation of WW ftol

--- src/Misc/Hooks.CRT.h
+++ src/Misc/Hooks.CRT.h
@@ -2,10 +2,16 @@
 
 struct CRTHooks{
 	static void Apply();
-
+	static void Print_FPUMode();
 protected:
 	static void ApplyMallocHooks();
 	static void ApplyFreeHooks();
 	static void ApplyMathHooks();
 	static void ApplyftolHooks();
+
+public :
+
+	static unsigned short g_cached_game_codeword;
+
+	static void Initializeftol();
 };
