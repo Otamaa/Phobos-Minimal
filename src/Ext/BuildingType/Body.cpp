e373c2a26b432156f66dca2666332d908851d6d8
Otamaa <otamaindodapuq2@gmail.com>
2026-09-13 01:53:01 +0700

    - bruh

--- src/Ext/BuildingType/Body.cpp
+++ src/Ext/BuildingType/Body.cpp
@@ -1834,7 +1834,7 @@ bool BuildingTypeExtData::LoadFromINI(CCINIClass* pINI, bool parseFailAddr)
 		this->RallyPointSpeedType.Read(exINI, pSection, "RallySpeedType");
 		this->RallyPointMovementZone.Read(exINI, pSection, "RallyMovementZone");
 
-		this->DisableProductionDuringBuildup.read(exINI, pSection, "DisableProductionDuringBuildup");
+		this->DisableProductionDuringBuildup.Read(exINI, pSection, "DisableProductionDuringBuildup");
 
 		if (this->DisplayIncome_Delay.isset() && this->DisplayIncome_Delay.Fetch() == 0) {
 			Debug::Log("[Developer warning] [%s] DisplayIncome.Delay is set to 0, forcing to 1.\n", pSection);
